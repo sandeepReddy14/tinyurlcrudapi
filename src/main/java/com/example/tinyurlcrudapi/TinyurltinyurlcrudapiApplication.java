65e9a288e249f4078f05fe921212f8909e1827e5
sandeepReddy14 <60684412+sandeepReddy14@users.noreply.github.com>
2023-07-10 22:04:49 +0530

    Initial project setup

---
+++ src/main/java/com/example/tinyurlcrudapi/TinyurltinyurlcrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyurlcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurltinyurlcrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurltinyurlcrudapiApplication.class, args);
+	}
+
+}
